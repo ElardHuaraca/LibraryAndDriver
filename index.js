bc5b5123bf35c4aca8c9d60b3a7bd2238abbb11d
ElardHuaraca <elard.sd.9@gmail.com>
2023-01-25 22:33:31 -0500

    Change async to fuction

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import puppeteer from 'puppeteer'
 
+const accesPage = async () => {
 
-(async () => {
-
-    const browser = await puppeteer.launch({ headless: false })
+    const browser = await puppeteer.launch({ headless: false, args: ['--window-size=1280,720'] })
     const page = await browser.newPage()
     await page.goto("http://10.0.88.34/login.php")
+
+    const page_load_1 = page.waitForNavigation({ waitUntil: 'networkidle0' })
+    const page_load_2 = page.waitForNavigation({ waitUntil: 'networkidle2' })
 
     /* Authenticate and user selected in list*/
     await page.waitForSelector('#slctAccount_chosen')
@@ -18,15 +20,37 @@ import puppeteer from 'puppeteer'
     await page.type("#logPwd", "password2023*")
     await page.click("#BTNLOGIN")
 
-    /* Wait for the page to load */
-    await page.waitForNavigation({ waitUntil: 'networkidle2' })
+    /* wait page load content */
+
+
+    await page_load_2
+
+    /* Click on button for view status*/
+    await page.waitForSelector('#modStatus')
+    const btn_status = await page.$('#modStatus')
+    await btn_status.click()
 
     /* wait page load content */
+    await page_load_1
+
+    /* Click on button for view status drivers*/
+    await page.waitForSelector('#pgStatusDriveSettings')
+    const btn_driver_status = await page.$('#pgStatusDriveSettings')
+    await btn_driver_status.click()
+
+    /* wait page load content */
+    await new Promise(r => setTimeout(r, 5000))
+
     await page.waitForSelector('#contentDisplayMainArea')
 
+    await page.waitForSelector('#TBL_DRIVE')
+    const table = await page.$('#TBL_DRIVE')
+    const content = await table.$$('#drivenum')
 
     /* Take a screenshot */
     await page.screenshot({ path: "google.png" })
     await browser.close()
 
-})()
+}
+
+accesPage()
