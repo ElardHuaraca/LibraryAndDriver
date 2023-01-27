a1a177db10b4c454d11d42f2bafd76a673cc9a15
ElardHuaraca <elard.sd.9@gmail.com>
2023-01-27 15:40:07 -0500

    Update main index.js

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 import AccesPage from './src/AccesPage.js'
+import fs from 'fs'
 
-AccesPage()
+/* read file json localy */
+fs.readFile('./drivers.json', 'utf8', async (err, data) => {
+})
+
+const listDrivers = await AccesPage()
+
+console.log(listDrivers)
