e457f63e089301e6a04739d64a795fedfdb684f2
ElardHuaraca <elard.sd.9@gmail.com>
2023-01-30 13:56:02 -0500

    Implement new metod to read file Rename function read file from json Implement function write file first part

--- src/ReadAndCreateFile.js
+++ src/ReadAndCreateFile.js
@@ -1,13 +1,26 @@
 import fs from 'fs'
 
+export function ReadFile(filename) {
+    return fs.readFileSync(filename).toString()
+}
+
 /* Read File libraries.json localy in project */
-export function ReadFile() {
+export function ReadFileFromJson() {
     const string_content = fs.readFileSync('libraries.json').toString()
     return JSON.parse(string_content)
 }
 
 /* Write new file .html and content libraries information */
-export function WriteFile() {
+export function WriteFile(listLibrary) {
+    const file = fs.createWriteStream('libraries.html')
+    file.on('ready', () => {
+        file.write('<html><head><title>Libraries</title></head><body>')
+        file.write('<table>')
+
+        file.write('</table>')
+        file.write('</body></html>')
+        file.end()
+    })
 }
 
 /* Delete file created */
