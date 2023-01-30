8608b695dd498d0213c84a503858cf8b18215a1e
ElardHuaraca <elard.sd.9@gmail.com>
2023-01-30 15:46:57 -0500

    Implement writeFile function

--- src/ReadAndCreateFile.js
+++ src/ReadAndCreateFile.js
@@ -14,9 +14,27 @@ export function ReadFileFromJson() {
 export function WriteFile(listLibrary) {
     const file = fs.createWriteStream('libraries.html')
     file.on('ready', () => {
-        file.write('<html><head><title>Libraries</title></head><body>')
-        file.write('<table>')
-
+        file.write('<!DOCTYPE html>')
+        file.write('<html lang="en">')
+        file.write('<head><title>Libraries</title>')
+        file.write('<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">')
+        file.write('</head><body>')
+        file.write('<table class="table table-bordered border-dark text-center">')
+        file.write('<thead><tr><th scope="col">Libreria</th><th scope="col">Drives</th><th scope="col">Estatus</th><th scope="col">Proceso</th><th scope="col">IP</th></tr></thead>')
+        file.write('<tbody>')
+        listLibrary.forEach((item) => {
+            const { library, drivers } = item
+            file.write(`<tr><td rowspan="${drivers.length}" class="align-middle">${library.name}</td>`)
+            drivers.forEach((driver, index) => {
+                if (index > 0) file.write('<tr>')
+                file.write(`<td>Drive ${driver.id}</td>`)
+                file.write(`<td>${driver.status}</td>`)
+                file.write(`<td>${driver.process}</td>`)
+                if (index == 0) file.write(`<td rowspan="${drivers.length}" class="align-middle"><a href="http://${library.ip}">${library.ip}</a></td>`)
+                file.write('</tr>')
+            })
+        })
+        file.write('</tbody>')
         file.write('</table>')
         file.write('</body></html>')
         file.end()
