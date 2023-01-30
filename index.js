14e56bd4eda97e5ab33b6d77a41e4813fe9561a9
ElardHuaraca <elard.sd.9@gmail.com>
2023-01-30 13:45:15 -0500

    Now array content is library an drivers

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 import AccesPage from './src/AccesPage.js'
-import { ReadFile } from './src/ReadAndCreateFile.js'
+import { ReadFile, WriteFile } from './src/ReadAndCreateFile.js'
 
 /* read file json localy */
 const libraries = ReadFile()
 
-libraries.forEach(async (library) => {
-    const listDrivers = await AccesPage(library.ip)
-})
+/* Loop array content and get information by library */
+const array = await Promise.all(
+    libraries.map(async (library) => {
+        return { library: library, drivers: await AccesPage(library.ip) }
+    })
+)
+
+WriteFile(array)
