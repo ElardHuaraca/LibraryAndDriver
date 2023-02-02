0eeef211d74f31c4f75f60318bfd564b8edae6f9
ElardHuaraca <elard.sd.9@gmail.com>
2023-02-02 13:04:00 -0500

    Implement column serial number and on/of

--- src/ReadAndCreateFile.js
+++ src/ReadAndCreateFile.js
@@ -16,16 +16,19 @@ export async function WriteFile(listLibrary) {
     file.on('ready', () => {
         file.write('<!DOCTYPE html><html><head><meta charset="utf-8"><title>Libraries</title></head><body>')
         file.write('<table style="border-collapse:collapse; width:100%;text-align:center;" border="1">')
-        file.write('<thead><tr><th scope="col">Libreria</th><th scope="col">Drives</th><th scope="col">Estatus</th><th scope="col">Proceso</th><th scope="col">IP</th></tr></thead>')
+        file.write('<thead><tr><th scope="col">Libreria</th><th scope="col">Drives</th><th scope="col">Serial</th><th scope="col">Estatus</th><th scope="col">Proceso</th><th scope="col">On/Of</th><th scope="col">IP</th></tr></thead>')
         file.write('<tbody>')
         listLibrary.forEach((item) => {
             const { library, drivers } = item
             file.write(`<tr><td rowspan="${drivers.length}">${library.name}</td>`)
+
             drivers.forEach((driver, index) => {
                 if (index > 0) file.write('<tr>')
                 file.write(`<td>Drive ${driver.id}</td>`)
+                file.write(`<td>${driver.serial}</td>`)
                 file.write(`<td>${driver.status}</td>`)
                 file.write(`<td>${driver.process}</td>`)
+                file.write(`<td>${driver.powerfull}</td>`)
                 if (index == 0) file.write(`<td rowspan="${drivers.length}"><a href="http://${library.ip}">${library.ip}</a></td>`)
                 file.write('</tr>')
             })
