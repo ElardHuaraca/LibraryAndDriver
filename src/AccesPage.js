0b45f4774bbab57fb29a8f76adf042e277c23d1c
ElardHuaraca <elard.sd.9@gmail.com>
2023-02-24 23:17:15 -0500

    Implement support library 4048 and 4300

--- src/AccesPage.js
+++ src/AccesPage.js
@@ -4,6 +4,7 @@ const STATUS = {
     'DRIVE_STATUS': 'N.A.',
     'DRIVE_STATUS OKbuttonDriveBullet OK': 'OK',
     'Ready': 'OK',
+    'Writing': 'OK',
     'DEFAULT': 'WARNING',
 }
 
@@ -16,27 +17,33 @@ const URL_BY_VERSION = (version, ip) => {
             return `http://${ip}/login.php`
         case '2024':
             return `http://${ip}/login.ssi`
+        case '4048':
+            return `http://${ip}/login.ssi`
+        default:
+            return `http://${ip}/login.php`
     }
 }
 
 const LIBRARIES_VERSION = async (...args) => {
-    const [page, user_, password_, page_load_2, page_load_1, sleep, ListDrivers, browser, version] = args
+    const [page, user_, password_, page_load_2, page_load_1, sleep, browser, version] = args
 
     switch (version) {
         case '6480':
-            return await Libary6480(page, user_, password_, page_load_2, page_load_1, sleep, ListDrivers, browser,)
+            return await Libary6480(page, user_, password_, page_load_2, page_load_1, sleep, browser)
         case '3040':
-            return await Libary6480(page, user_, password_, page_load_2, page_load_1, sleep, ListDrivers, browser,)
+            return await Libary6480(page, user_, password_, page_load_2, page_load_1, sleep, browser)
         case '2024':
-            return await Libary2024(page, user_, password_, page_load_2, page_load_1, sleep, ListDrivers, browser,)
+            return await Libary2024(page, user_, password_, page_load_2, page_load_1, sleep, browser)
+        case '4048':
+            return await Libary2024(page, user_, password_, page_load_2, page_load_1, sleep, browser)
+        case '4300':
+            return await Libary4300(page, user_, password_, page_load_2, page_load_1, sleep, browser)
     }
 }
 
 const AccesPage = async (ip, user_, password_, version_) => {
 
-    const ListDrivers = []
-
-    const browser = await puppeteer.launch({ headless: true, args: ["--window-size=1366,720", "--fast-start", "--disable-extensions", "--no-sandbox"] })
+    const browser = await puppeteer.launch({ headless: false, args: ["--window-size=1366,720", "--fast-start", "--disable-extensions", "--no-sandbox"] })
     const page = await browser.newPage()
     try {
         await page.goto(URL_BY_VERSION(version_, ip))
@@ -57,18 +64,19 @@ const AccesPage = async (ip, user_, password_, version_) => {
     const sleep = async (ms = 1000) => await new Promise(r => setTimeout(r, ms))
 
     /* Authenticate and user selected in list*/
-    return await LIBRARIES_VERSION(page, user_, password_, page_load_2, page_load_1, sleep, ListDrivers, browser, version_)
+    return await LIBRARIES_VERSION(page, user_, password_, page_load_2, page_load_1, sleep, browser, version_)
 }
 
-async function Libary6480(page, user_, password_, page_load_2, page_load_1, sleep, ListDrivers, browser) {
+async function Libary6480(page, user_, password_, page_load_2, page_load_1, sleep, browser) {
+    const ListDrivers = []
     try { await page.waitForSelector('#slctAccount_chosen', { timeout: 5000 }) }
     catch (e) { }
 
     let div = await page.$('#slctAccount_chosen')
     div ??= await page.$('#slctAccount')
-    div == null ? '' : await div.click()
 
     if (div != null) {
+        await div.click()
         const username = await page.$$('.group-option')
         for (let index = 0; index < username.length; index++) {
             const user = await (await username[index].getProperty('innerText')).jsonValue()
@@ -109,38 +117,64 @@ async function Libary6480(page, user_, password_, page_load_2, page_load_1, slee
     if (open)
         await (await open.$('.open-close-button')).click()
 
-    table.$$('#drivenum').then(rows => {
-        rows.forEach(async (row) => {
-            const div_log_num = await row.$('#DRIVE_LOG_NUM')
-            const div_status = await row.$('#DRIVE_STATUS')
-            const div_activity = await row.$('#DRIVE_ACTIVITY')
-            const div_enable = await row.$('#DRIVE_ENABLED_HEAD')
-            const div_serial = await row.$('#DRIVE_SERIAL_NO')
+    const columns = await table.$$('#drivenum')
+    const list_ = await Promise.all(columns.map(async (row) => {
+        let object = {}
+        const div_log_num = await row.$('#DRIVE_LOG_NUM')
+        const div_status = await row.$('#DRIVE_STATUS')
+        const div_activity = await row.$('#DRIVE_ACTIVITY')
+        const div_enable = await row.$('#DRIVE_ENABLED_HEAD')
+        const div_serial = await row.$('#DRIVE_SERIAL_NO')
 
-            const text_num = await (await div_log_num.getProperty('textContent')).jsonValue()
-            const text_class = await (await div_status.getProperty('className')).jsonValue()
-            const text_activity = await (await div_activity.getProperty('textContent')).jsonValue()
-            const text_enable = await (await div_enable.getProperty('textContent')).jsonValue()
-            const text_serial = await (await div_serial.getProperty('textContent')).jsonValue()
+        const text_num = await (await div_log_num.getProperty('textContent')).jsonValue()
+        const text_class = await (await div_status.getProperty('className')).jsonValue()
+        const text_activity = await (await div_activity.getProperty('textContent')).jsonValue()
+        const text_enable = await (await div_enable.getProperty('textContent')).jsonValue()
+        const text_serial = await (await div_serial.getProperty('textContent')).jsonValue()
 
-            ListDrivers.push({
-                id: text_num,
-                status: STATUS[text_class] || STATUS['DEFAULT'],
-                process: text_activity === ' ' ? 'N.A.' : text_activity,
-                powerfull: text_enable,
-                serial: text_serial,
-            })
+        object = {
+            id: text_num,
+            status: STATUS[text_class] || STATUS['DEFAULT'],
+            process: text_activity === ' ' ? 'N.A.' : text_activity,
+            powerfull: text_enable === ' ' ? 'N.A.' : text_enable,
+            serial: text_serial === ' ' ? 'N.A.' : text_serial
+        }
+
+        return object
+    }))
+    /* .then(rows => {
+    const list = []
+    console.log(rows)
+    rows.forEach(async (row) => {
+        console.log(row)
+        const div_log_num = await row.$('#DRIVE_LOG_NUM')
+        const div_status = await row.$('#DRIVE_STATUS')
+        const div_activity = await row.$('#DRIVE_ACTIVITY')
+        const div_enable = await row.$('#DRIVE_ENABLED_HEAD')
+        const div_serial = await row.$('#DRIVE_SERIAL_NO')
+
+        const text_num = await (await div_log_num.getProperty('textContent')).jsonValue()
+        const text_class = await (await div_status.getProperty('className')).jsonValue()
+        const text_activity = await (await div_activity.getProperty('textContent')).jsonValue()
+        const text_enable = await (await div_enable.getProperty('textContent')).jsonValue()
+        const text_serial = await (await div_serial.getProperty('textContent')).jsonValue()
+
+        list.push({
+            id: text_num,
+            status: STATUS[text_class] || STATUS['DEFAULT'],
+            process: text_activity === ' ' ? 'N.A.' : text_activity,
+            powerfull: text_enable,
+            serial: text_serial,
         })
     })
-
-    /* Take a screenshot */
-    await page.screenshot({ path: "google.png" })
-    await browser.close()
-
-    return ListDrivers
+    console.log(list)
+    return list
+}) */
+    return ListDrivers.concat(list_)
 }
 
-async function Libary2024(page, user_, password_, page_load_2, page_load_1, sleep, ListDrivers, browser) {
+async function Libary2024(page, user_, password_, page_load_2, page_load_1, sleep, browser) {
+    const ListDrivers = []
 
     await page.waitForSelector('#ButtonRegion')
 
@@ -212,9 +246,54 @@ async function Libary2024(page, user_, password_, page_load_2, page_load_1, slee
         }
     })
 
-    /* Take a screenshot */
-    await page.screenshot({ path: "google.png" })
-    await browser.close()
+    return ListDrivers
+}
+
+async function Libary4300(page, user_, password_, page_load_2, page_load_1, sleep, browser) {
+    const ListDrivers = []
+    try { await page.waitForSelector('#slctAccount', { timeout: 5000 }) }
+    catch (e) { }
+
+    await page.type('#slctAccount', user_ || process.env.USERNAME_LIB)
+    await page.type('#logPwd', password_ || process.env.PASSWORD_LIB)
+    await page.click('#BTNLOGIN')
+
+    /* wait page load content */
+    await page_load_2
+
+    await page.waitForSelector('#mnuDrives')
+    const btn_drivers = await page.$('#mnuDrives')
+    await btn_drivers.click()
+
+    await page_load_1
+    await sleep(3000)
+
+    await page.waitForSelector('#TBL_DRIVE')
+    const table = await page.$('#TBL_DRIVE')
+
+    table.$$('#drivenum').then(rows => {
+        rows.forEach(async (row) => {
+            const div_log_num = await row.$('#DRIVE_LOG_NUM')
+            const div_status = await row.$('#DRIVE_STATUS')
+            const div_activity = await row.$('#DRIVE_ACTIVITY')
+            const div_enable = await row.$('#DRIVE_ENABLED_HEAD')
+            const div_serial = await row.$('#DRIVE_SERIAL_NO')
+
+            const text_num = await (await div_log_num.getProperty('textContent')).jsonValue()
+            const text_class = await (await div_status.getProperty('className')).jsonValue()
+            const text_activity = await (await div_activity.getProperty('textContent')).jsonValue()
+            const text_enable = await (await div_enable.getProperty('textContent')).jsonValue()
+            const text_serial = await (await div_serial.getProperty('textContent')).jsonValue()
+
+            ListDrivers.push({
+                id: text_num,
+                status: STATUS[text_class] || STATUS['DEFAULT'],
+                process: text_activity === ' ' ? 'N.A.' : text_activity,
+                powerfull: text_enable,
+                serial: text_serial,
+            })
+        })
+    })
 
     return ListDrivers
 }
